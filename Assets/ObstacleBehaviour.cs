cf915a3e627aa03613f5e8e41dbfdcd4246c1f9e
kumankoishi.py <7iggif@naver.com>
2020-01-22 19:39:09 +0900

    Initial Commit (Implemented Rail Rotation)

---
+++ Assets/ObstacleBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0f, 0f, speed);
+        if(transform.position.z < -18f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 20f);
+        }
+    }
+}
---
+++ Assets/RailController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            RotateRail(-1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            RotateRail(1);
+        }
+    }
+
+    void RotateRail(int direction)
+    {
+        // -1 left, 1 right
+        if(direction == -1 || direction == 1)
+        {
+            transform.Rotate(new Vector3(0f, 45f * direction, 0f), Space.Self);
+        }
+    }
+}
