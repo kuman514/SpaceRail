34ed5bf7e29e40d1938a092c7cd792c70da0987c
kumankoishi.py <7iggif@naver.com>
2020-01-29 16:18:12 +0900

    Implemented Game Over UI (Issue: the game doesn't stop even if the game is over)

---
+++ Assets/GameOverUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverUI : MonoBehaviour
+{
+    bool isGameOver;
+    public GameObject Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // If the game over UI is inactive, it doesn't activate itself in any condition.
+        // therefore, I made a new empty object, GameManager,
+        // and I make it manage a target, the game over UI.
+
+        isGameOver = false;
+        Target.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Activate Game Over UI
+        if(PlayerController.Lives <= 0 && !isGameOver)
+        {
+            isGameOver = true;
+            Target.SetActive(true);
+        }
+    }
+}
