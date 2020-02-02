5e3eb1f83da34f1ee7bcca7034d49c21ceb8dcbf
kumankoishi.py <7iggif@naver.com>
2020-02-02 14:33:59 +0900

    added rotate sound

--- Assets/RailController.cs
+++ Assets/RailController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class RailController : MonoBehaviour
 {
     Rigidbody rb;
+    AudioSource se;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        se = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,6 +33,7 @@ public class RailController : MonoBehaviour
         // -1 left, 1 right
         if(direction == -1 || direction == 1)
         {
+            se.Play();
             transform.Rotate(new Vector3(0f, 45f * direction, 0f), Space.Self);
         }
     }
